2439c7eb04148dac9b9290d1afef7c9fd45d7ac0
AnaClaudiaIR <anac.ihnrod@educa.jcyl.es>
2026-05-22 18:07:59 +0200

    subir2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,26 @@
 package org.example;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+//import java.awt.*;
+//import java.awt.TextField; --> No se necesitan estos imports
+import javafx.scene.control.*;
+
+import java.util.Optional;
+
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Formulario con varios botones");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/org/example/c.java
+++ src/main/java/org/example/c.java
@@ -1,4 +1,19 @@
 package org.example;
 
+import java.util.Locale;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class c {
+    public static void main(String[] args) {
+
+        List<String> nombresPaises = Arrays.stream(Locale.getISOCountries())
+                .map(codigo -> new Locale("", codigo).getDisplayCountry(new Locale("es", "ES")))
+                .sorted()
+                .collect(Collectors.toList());
+
+        // Imprimir solo los nombres
+        nombresPaises.forEach(System.out::println);
+    }
 }
